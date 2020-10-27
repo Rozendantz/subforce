6508eefd29fa8499d3c4344f2dc721bc215eb442
denegtheworld <admin@bitshift.net.au>
2020-10-27 20:47:13 +1100

    add and randomize selection of user agents

--- subforce.py
+++ subforce.py
@@ -5,6 +5,8 @@ import linecache
 import json
 import mechanicalsoup as ms
 from bs4 import BeautifulSoup as bs
+import random
+
 
 from subforce import sub_file
 from subforce import dir_file
@@ -32,6 +34,65 @@ stack_size = 100
 
 browser = []
 
+
+user_agents_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+    'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+    'Mozilla/5.0 CK={} (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; KTXN)',
+    'Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+    'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko)',
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1; 125LA; .NET CLR 2.0.50727; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022)',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.83 Safari/537.1',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393',
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows 98)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36']
+
+
 def files_exist(subfile, dirfile):
     if os.path.isfile(subfile):
         subfile_exist = True
@@ -53,7 +114,6 @@ async def read_from_file(list_file, file_lines, read_stack, file_iterator):
     global sleep_inc
 
     if file_iterator[-1] == file_lines -1:
-        print(' *** TEST: file_iterator')
         return
 
     if len(read_stack) < stack_size -1:
@@ -76,9 +136,6 @@ async def concat_addr(subread, dirread):
     global subfile_lines
     global dirfile_lines
 
-    #print('subfile_lines: {}',subfile_lines.result())
-    #print('dirfile_lines: {}',dirfile_lines.result())
-
     domains_list_size = len(domains_list)
     domains_remainder = stack_size - domains_list_size
 
@@ -90,47 +147,25 @@ async def concat_addr(subread, dirread):
     else:
         await asyncio.sleep(sleep_inc)
 
-'''
-async def get_http(domain):
-    global results_list
-    global domains_list
-    global sleep_inc
-    global browser
-    browser.append(ms.Browser(session=None, soup_config={'features': 'lxml'}, requests_adapters=None, raise_on_404=False, user_agent=None))
-    if len(domains_list) > 1:
-        print('http://'+domains_list[-1])
-        results_list.append(browser[-1].get('http://'+domains_list.pop()))
-        await asyncio.sleep(sleep_inc + 1)
-    else:
-        await asyncio.sleep(sleep_inc + 1)
 
+def swap_user_agent():
+    global user_agents_list
+    rand_num = random.randrange(len(user_agents_list))
+    print(user_agents_list[rand_num])
+    return user_agents_list[rand_num]
 
-async def get_https(domain):
-    global results_list
-    global domains_list
-    global sleep_inc
-    browser.append(ms.Browser(session=None, soup_config={'features': 'lxml'}, requests_adapters=None, raise_on_404=False, user_agent=None))
-    if len(domains_list) > 0:
-        print('https://'+domains_list.pop())
-        results_list.append(browser[-1].get('https://'+domains_list.pop()))
-        await asyncio.sleep(sleep_inc + 1)
-    else:
-        await asyncio.sleep(sleep_inc + 1)
-'''
 
 async def get_url(domain):
     global results_list
     global domains_list
     global sleep_inc
-    browser.append(ms.Browser(session=None, soup_config={'features': 'lxml'}, requests_adapters=None, raise_on_404=False, user_agent=None))
+    agent = swap_user_agent()
+    browser.append(ms.Browser(session=None, soup_config={'features': 'lxml'}, requests_adapters=None, raise_on_404=False, user_agent=agent))
     if len(domains_list) > 0:
-        #print(domains_list.pop())
         results_list.append(browser[-1].get('http://'+domains_list.pop()+'?'))
         await asyncio.sleep(sleep_inc + 1)
     else:
         await asyncio.sleep(sleep_inc + 1)
-
-
 
 
 async def get_lines(list_file):
@@ -147,15 +182,10 @@ async def file_lines():
     global subfile_lines
     global dirfile_lines
 
-    #print(sub_file)
-    #print(dir_file)
-    #print('check_lines')
     if files_exist(sub_file, dir_file):
         subfile_lines = files_loop.create_task(get_lines(sub_file))
         dirfile_lines = files_loop.create_task(get_lines(dir_file))
         await asyncio.wait([subfile_lines, dirfile_lines])
-        #print('subfile_lines = {}'.format(subfile_lines.result()))
-        #print('dirfile_lines = {}'.format(dirfile_lines.result()))
 
 
 async def load_files():
@@ -180,23 +210,16 @@ async def get():
 
     for ip in domains_list:
         get = main_loop.create_task(get_url(domains_list))
-        #http = main_loop.create_task(get_http(domains_list))
-        #https = main_loop.create_task(get_https(domains_list))
-    #await asyncio.wait([http, https])
     await asyncio.wait([get])
 
     file = open('results.txt', 'a')
-    print(results_list)
     for result in results_list:
-        print(result.url)
-        print(result.headers)
-        print(result.cookies)
-
         headers = result.headers
         cookiejar = result.cookies
         cookies = cookiejar.items()
-
-        file.write("test\n")
+        print(f"{result.status_code: <1} - {result.url:^1}")
+        file.write("\n\n")
+        file.write("***************************************************************************************************************************************\n")
         file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
         file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
         file.write("                                                           {}                                                                          \n".format(result.url))
@@ -220,6 +243,8 @@ async def get():
         file.write("\t{}".format(html_formatted))
         file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
         file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
+        file.write("***************************************************************************************************************************************\n")
+        file.write("\n")
 
     file.close()
     for session in browser:
@@ -240,10 +265,7 @@ async def main():
         await load_files()
         await get()
         if check_domains_list():
-            print('domains_list low')
             await load_files()
-
-    print(results_list)
 
 
 if __name__ == "__main__":
