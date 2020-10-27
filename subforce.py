bfe340b75f04baab0a96249712806d5afdcfb9c7
denegtheworld <admin@bitshift.net.au>
2020-10-27 18:13:32 +1100

    finished formatting the logs

--- subforce.py
+++ subforce.py
@@ -2,7 +2,9 @@ import os.path
 import sys
 import asyncio
 import linecache
+import json
 import mechanicalsoup as ms
+from bs4 import BeautifulSoup as bs
 
 from subforce import sub_file
 from subforce import dir_file
@@ -28,6 +30,8 @@ dirfile_lines = 0
 sleep_inc = 0.01
 stack_size = 100
 
+browser = []
+
 def files_exist(subfile, dirfile):
     if os.path.isfile(subfile):
         subfile_exist = True
@@ -48,15 +52,14 @@ def files_exist(subfile, dirfile):
 async def read_from_file(list_file, file_lines, read_stack, file_iterator):
     global sleep_inc
 
-    if file_iterator[0] == file_lines -1:
+    if file_iterator[-1] == file_lines -1:
         print(' *** TEST: file_iterator')
         return
 
     if len(read_stack) < stack_size -1:
         with open(list_file) as f:
-            for i in range(1, file_lines):
-                file_iterator[0] = i
-                read_stack.append(linecache.getline(list_file, file_iterator[0], module_globals=None).strip())
+            for i in range(1, file_lines-1):
+                read_stack.append(linecache.getline(list_file, i, module_globals=None).strip())
                 await asyncio.sleep(sleep_inc)
                 if i == stack_size:
                     await asyncio.sleep(sleep_inc)
@@ -87,16 +90,47 @@ async def concat_addr(subread, dirread):
     else:
         await asyncio.sleep(sleep_inc)
 
-
-async def ping_address(domain):
+'''
+async def get_http(domain):
     global results_list
     global domains_list
     global sleep_inc
-    if len(domains_list) > 0:
-        print('ip: {}'.format(domains_list.pop()))
+    global browser
+    browser.append(ms.Browser(session=None, soup_config={'features': 'lxml'}, requests_adapters=None, raise_on_404=False, user_agent=None))
+    if len(domains_list) > 1:
+        print('http://'+domains_list[-1])
+        results_list.append(browser[-1].get('http://'+domains_list.pop()))
         await asyncio.sleep(sleep_inc + 1)
     else:
         await asyncio.sleep(sleep_inc + 1)
+
+
+async def get_https(domain):
+    global results_list
+    global domains_list
+    global sleep_inc
+    browser.append(ms.Browser(session=None, soup_config={'features': 'lxml'}, requests_adapters=None, raise_on_404=False, user_agent=None))
+    if len(domains_list) > 0:
+        print('https://'+domains_list.pop())
+        results_list.append(browser[-1].get('https://'+domains_list.pop()))
+        await asyncio.sleep(sleep_inc + 1)
+    else:
+        await asyncio.sleep(sleep_inc + 1)
+'''
+
+async def get_url(domain):
+    global results_list
+    global domains_list
+    global sleep_inc
+    browser.append(ms.Browser(session=None, soup_config={'features': 'lxml'}, requests_adapters=None, raise_on_404=False, user_agent=None))
+    if len(domains_list) > 0:
+        #print(domains_list.pop())
+        results_list.append(browser[-1].get('http://'+domains_list.pop()+'?'))
+        await asyncio.sleep(sleep_inc + 1)
+    else:
+        await asyncio.sleep(sleep_inc + 1)
+
+
 
 
 async def get_lines(list_file):
@@ -123,6 +157,7 @@ async def file_lines():
         #print('subfile_lines = {}'.format(subfile_lines.result()))
         #print('dirfile_lines = {}'.format(dirfile_lines.result()))
 
+
 async def load_files():
     global sub_file
     global dir_file
@@ -139,11 +174,56 @@ async def load_files():
     await asyncio.wait([read_from_sub_file, read_from_dir_file, concat_sub_to_dir])
 
 
-async def ping():
-        for ip in domains_list:
-            ping_addr = main_loop.create_task(ping_address(domains_list))
-        await asyncio.wait([ping_addr])
+async def get():
+    global browser
+    global domains_list
 
+    for ip in domains_list:
+        get = main_loop.create_task(get_url(domains_list))
+        #http = main_loop.create_task(get_http(domains_list))
+        #https = main_loop.create_task(get_https(domains_list))
+    #await asyncio.wait([http, https])
+    await asyncio.wait([get])
+
+    file = open('results.txt', 'a')
+    print(results_list)
+    for result in results_list:
+        print(result.url)
+        print(result.headers)
+        print(result.cookies)
+
+        headers = result.headers
+        cookiejar = result.cookies
+        cookies = cookiejar.items()
+
+        file.write("test\n")
+        file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
+        file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
+        file.write("                                                           {}                                                                          \n".format(result.url))
+        file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
+        file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
+        file.write("- status: {}\n".format(result.status_code))
+        file.write("- reason: {}\n".format(result.reason))
+        file.write("- is redirect? {}\n".format(result.is_redirect))
+        if result.is_redirect:
+            file.write("is permanent redirect? {}\n".format(result.is_permanent.redirect))
+        file.write("- headers: \n")
+        for key,value in headers.items():
+            file.write("\t{keys}: {values}\n".format(keys=key, values=value))
+        #file.write("headers_links: {}\n".format(header_links))
+        file.write("- cookies: \n")
+        for cookie in cookies:
+            file.write("\t{}\n".format(cookie))
+        result_bytes = result.content
+        html_formatted = result_bytes.decode('utf-8')
+        file.write("- content: \n")
+        file.write("\t{}".format(html_formatted))
+        file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
+        file.write("---------------------------------------------------------------------------------------------------------------------------------------\n")
+
+    file.close()
+    for session in browser:
+        session.close()
 
 def check_domains_list():
     if len(domains_list) < (stack_size -1) / 2:
@@ -152,24 +232,19 @@ def check_domains_list():
 
 async def main():
     global domains_list
+    global results_list
     x = 0
     y = 0
 
     if files_exist(sub_file, dir_file):
         await load_files()
-        await ping()
+        await get()
         if check_domains_list():
             print('domains_list low')
             await load_files()
 
-        '''
-        print('substack:')
-        print(subfile_readstack)
-        print('dirstack:')
-        print(dirfile_readstack)
-        print('domains_list:')
-        print(domains_list)
-        '''
+    print(results_list)
+
 
 if __name__ == "__main__":
     try:
@@ -181,17 +256,17 @@ if __name__ == "__main__":
         main_loop.run_until_complete(main())
     except Exception as e:
         pass
-        print(e)
+        #print(e)
     finally:
-        main_loop.close()
         files_loop.close()
+        main_loop.close()
 
 # enumerate through sublist file and add sub folders from dirlist file
 # do this inside a pool and make sure it starts firing requests incrementally as we move through the files
 
 # NOTES:
 # pop as you go along each list
-# make sure to mimic a browser when doing ping
+# make sure to mimic a browser when doing get
 # read http codes 
 # format output file
 # set default output file
@@ -200,3 +275,5 @@ if __name__ == "__main__":
 # instruct ppl to use this tool with proxychains for more discrete operation
 # check for robots.txt first!!
 # fix the subfile/dirfile_iterator problem, try using an array
+# option to open directory tree in w3m???
+# add option for configuring ms.Browser()
