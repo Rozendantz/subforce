2d4919137de1d1d4f1a4d7c72233f678d4c1d2ad
denegtheworld <admin@bitshift.net.au>
2020-10-31 14:04:02 +1100

    moved notes to README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# NOTES:
+# make sure to mimic a browser when doing get... DONE
+# read http codes ... DONE
+# format output file ... DONE
+# set default output file ... DONE
+# random delays between requests ---
+# switch user agents... DONE
+# instruct ppl to use this tool with proxychains for more discrete operation
+# check for robots.txt first!!
+# fix the subfile/dirfile_iterator problem, try using an array... DONE
+# option to open directory tree in w3m??? (maybe dump content into individual files that can be opened)
+# add option for configuring ms.Browser() ----
+# add proxy support
+# add content discovery and possible fuzzing
+# add database support: mysql? sqllite?
+# add semaphores for using huge files ----
+
--- subforce.py
+++ subforce.py
@@ -308,21 +308,3 @@ if __name__ == "__main__":
         files_write_loop.close()
 
 
-# enumerate through sublist file and add sub folders from dirlist file
-# do this inside a pool and make sure it starts firing requests incrementally as we move through the files
-
-# NOTES:
-# make sure to mimic a browser when doing get... DONE
-# read http codes ... DONE
-# format output file ... DONE
-# set default output file ... DONE
-# random delays between requests ---
-# switch user agents... DONE
-# instruct ppl to use this tool with proxychains for more discrete operation
-# check for robots.txt first!!
-# fix the subfile/dirfile_iterator problem, try using an array... DONE
-# option to open directory tree in w3m??? (maybe dump content into individual files that can be opened)
-# add option for configuring ms.Browser() ----
-# add proxy support
-# add content discovery
-# add semaphores for using huge files
